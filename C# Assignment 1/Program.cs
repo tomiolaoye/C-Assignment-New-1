ea42fb45bdda7bf7085970bc052a474ecf000c52
OLUWATOMI OLAOYE <OLUWATOMI OLAOYE@WEMA-TRAINING-0>
2023-11-22 09:02:27 +0100

    Add project files.

---
+++ C# Assignment 1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Triangle Area Calculator");
+
+        // Get base and height input from the user
+        Console.Write("Enter the base of the triangle: ");
+        double triangleBase = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter the height of the triangle: ");
+        double height = Convert.ToDouble(Console.ReadLine());
+
+        // Calculate the area of the triangle
+        double area = CalculateTriangleArea(triangleBase, height);
+
+        // Display the result
+        Console.WriteLine($"The area of the triangle is: {area}");
+
+        // Wait for user input before closing the console window
+        Console.ReadLine();
+    }
+
+    static double CalculateTriangleArea(double triangleBase, double height)
+    {
+        // Formula for calculating the area of a triangle: Area = 0.5 * base * height
+        return 0.5 * triangleBase * height;
+    }
+}
